e4b31769d5f558d907b3d7f91149ee0cc01a26a6
Julian Commissaris <jpcommissaris@gmail.com>
2020-01-25 09:56:41 -0800

    server added

---
+++ server/server.js
@@ -0,0 +1,34 @@
+//https://socket.io/docs/emit-cheatsheet/
+
+const express = require('express');
+const app     = express();
+const http    = require('http').Server(app);
+const io  = require('socket.io')(http);
+const config  = require('./config.json');
+const MAX_CONNS = 8; 
+
+
+
+  
+
+//setup server
+app.use(express.static(__dirname + '/../client'));
+
+const serverPort = process.env.PORT || config.port;
+http.listen(serverPort, () => {
+  console.log("Server is listening on port " + serverPort);
+});
+
+
+io.on('connection', (socket) => {
+  console.log('New connection with id ' +socket.id)
+  
+});
+
+
+
+
+
+
+
+
